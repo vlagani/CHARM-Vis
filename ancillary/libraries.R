ad0da2b3cb176a55e4b585a59ccc251abd1a10af
vlagani <vincenzo.lagani@gmail.com>
2026-03-29 16:38:55 +0300

    Update libraries.R

--- ancillary/libraries.R
+++ ancillary/libraries.R
@@ -1,69 +1,85 @@
 # loading / installing libraries
 if(!require(pak)){
-  install.packages('pak')
+  install.packages('pak', quiet = TRUE)
   library(pak)
 }
+if(!require(BiocManager)){
+  install.packages('BiocManager', quiet = TRUE)
+  library(BiocManager)
+}
 if(!require(Seurat)){
-  pak('Seurat')
+  pak('Seurat', ask = FALSE)
   library(Seurat)
 }
 if(!require(tidyverse)){
-  pak('tidyverse')
+  pak('tidyverse', ask = FALSE)
   library(tidyverse)
 }
 if(!require(ggrepel)){
-  pak('ggrepel')
+  pak('ggrepel', ask = FALSE)
   library(ggrepel)
 }
 if(!require(openxlsx)){
-  pak('openxlsx')
+  pak('openxlsx', ask = FALSE)
   library(openxlsx)
 }
 if(!require(rtracklayer)){
-  pak('rtracklayer')
+  pak('rtracklayer', ask = FALSE)
   library(rtracklayer)
 }
 if(!require(DESeq2)){
-  pak('DESeq2')
+  pak('DESeq2', ask = FALSE)
   library(DESeq2)
 }
 if(!require(future)){
-  pak('future')
+  pak('future', ask = FALSE)
   library(future)
 }
+if(!require(doFuture)){
+  pak('doFuture', ask = FALSE)
+  library(doFuture)
+}
+if(!require(fgsea)){
+  BiocManager::install('fgsea', ask = FALSE, update = FALSE)
+  library(fgsea)
+}
+if(!require(DOSE)){
+  BiocManager::install('DOSE', ask = FALSE, update = FALSE)
+  library(DOSE)
+}
+if(!require(enrichplot)){
+  BiocManager::install('enrichplot', ask = FALSE, update = FALSE)
+  library(enrichplot)
+}
 if(!require(clusterProfiler)){
-  pak('clusterProfiler')
+  pak('clusterProfiler', ask = FALSE)
   library(clusterProfiler)
 }
 if(!require(enrichplot)){
-  pak('enrichplot')
+  pak('enrichplot', ask = FALSE)
   library(enrichplot)
 }
 if(!require(org.Mm.eg.db)){
-  pak('org.Mm.eg.db')
+  BiocManager::install('org.Mm.eg.db', ask = FALSE, update = FALSE)
   library(org.Mm.eg.db)
 }
-if(!require(doFuture)){
-  pak('doFuture')
-  library(doFuture)
-}
 if(!require(dittoSeq)){
-  pak('dittoSeq')
+  pak('dittoSeq', ask = FALSE)
   library(dittoSeq)
 }
 if(!require(SummarizedExperiment)){
-  pak('SummarizedExperiment')
+  pak('SummarizedExperiment', ask = FALSE)
   library(SummarizedExperiment)
 }
 if(!require(ComplexHeatmap)){
-  pak('ComplexHeatmap')
+  pak('ComplexHeatmap', ask = FALSE)
   library(ComplexHeatmap)
 }
 if(!require(circlize)){
-  pak('circlize')
+  pak('circlize', ask = FALSE)
   library(circlize)
 }
 if(!require(simplifyEnrichment)){
-  pak('simplifyEnrichment')
+  pak('simplifyEnrichment', ask = FALSE)
   library(simplifyEnrichment)
 }
